0025bce92ddbb76c8ae6a63311dc20859a2d4f0a
M3t0r <github@m3t0r.de>
2024-08-11 21:53:37 +0200

    Add CSV field ser/de for DNS/allowed_ips

--- src/wg.rs
+++ src/wg.rs
@@ -7,11 +7,11 @@ use std::{
 
 use base64ct::Encoding;
 use rand::RngCore;
-use serde::{de::Visitor, Deserialize, Serialize};
+use serde::{de::Visitor, Deserialize, Deserializer, Serialize, Serializer};
 
 const WG_KEY_LEN: usize = 32usize;
 type WGKeyBytes = [u8; WG_KEY_LEN];
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 pub struct WGPrivateKey(WGKeyBytes);
 impl WGPrivateKey {
     pub fn new() -> Self {
@@ -89,7 +89,7 @@ impl Debug for WGPrivateKey {
         self::Display::fmt(&self, f)
     }
 }
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 pub struct WGPublicKey(WGKeyBytes);
 impl From<&WGPrivateKey> for WGPublicKey {
     fn from(private: &WGPrivateKey) -> Self {
@@ -172,27 +172,26 @@ pub struct WGAddedKey {
     pub dns_servers: Vec<IpAddr>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct WGConf {
     pub interface: WGConfInterface,
     pub peer: WGConfPeer,
 }
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct WGConfInterface {
     pub address: IpAddr,
     pub private_key: WGPrivateKey,
-    // #[serde(rename = "DNS")]
-    // pub dns: Option<Vec<IpAddr>>,
+    #[serde(rename = "DNS", with = "optional_csv_vec", skip_serializing_if = "Option::is_none", default)]
+    pub dns: Option<Vec<IpAddr>>,
 }
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct WGConfPeer {
     pub public_key: WGPublicKey,
-    // #[serde(rename = "AllowedIPs")]
-    // pub allowed_ips: Vec<String>, // to lazy to write a cidr ser+de impl
-    pub allowed_ips: String,
+    #[serde(rename = "AllowedIPs", with = "csv_vec")]
+    pub allowed_ips: Vec<String>,
     pub endpoint: SocketAddr,
     pub hostname: Option<String>,
     pub persistent_keepalive: Option<u16>,
@@ -203,12 +202,12 @@ impl WGConf {
             interface: WGConfInterface {
                 address: server.client_vpn_ip,
                 private_key,
-                // dns: Some(server.dns_servers),
+                dns: Some(server.dns_servers),
             },
             peer: WGConfPeer {
                 public_key: server.server_key,
-                // allowed_ips: vec!["0.0.0.0/0".to_owned()],
-                allowed_ips: "0.0.0.0/0".to_owned(),
+                allowed_ips: vec!["0.0.0.0/0".to_owned()],
+                // allowed_ips: "0.0.0.0/0".to_owned(),
                 endpoint: SocketAddr::new(server.server_public_ip, server.server_port),
                 hostname: Some(server.server_name),
                 persistent_keepalive: Some(25),
@@ -237,40 +236,176 @@ impl WGConf {
     //     self.peer.persistent_keepalive.take();
     // }
     pub fn disable_dns(&mut self) {
-        //     self.interface.dns.take();
+        self.interface.dns.take();
     }
-    // fn restict_to_public_ips(&mut self) {
-    //     self.peer.allowed_ips = vec![
-    //         "0.0.0.0/5".to_owned(),
-    //         "8.0.0.0/7".to_owned(),
-    //         "11.0.0.0/8".to_owned(),
-    //         "12.0.0.0/6".to_owned(),
-    //         "16.0.0.0/4".to_owned(),
-    //         "32.0.0.0/3".to_owned(),
-    //         "64.0.0.0/2".to_owned(),
-    //         "128.0.0.0/3".to_owned(),
-    //         "160.0.0.0/5".to_owned(),
-    //         "168.0.0.0/6".to_owned(),
-    //         "172.0.0.0/12".to_owned(),
-    //         "172.32.0.0/11".to_owned(),
-    //         "172.64.0.0/10".to_owned(),
-    //         "172.128.0.0/9".to_owned(),
-    //         "173.0.0.0/8".to_owned(),
-    //         "174.0.0.0/7".to_owned(),
-    //         "176.0.0.0/4".to_owned(),
-    //         "192.0.0.0/9".to_owned(),
-    //         "192.128.0.0/11".to_owned(),
-    //         "192.160.0.0/13".to_owned(),
-    //         "192.169.0.0/16".to_owned(),
-    //         "192.170.0.0/15".to_owned(),
-    //         "192.172.0.0/14".to_owned(),
-    //         "192.176.0.0/12".to_owned(),
-    //         "192.192.0.0/10".to_owned(),
-    //         "193.0.0.0/8".to_owned(),
-    //         "194.0.0.0/7".to_owned(),
-    //         "196.0.0.0/6".to_owned(),
-    //         "200.0.0.0/5".to_owned(),
-    //         "208.0.0.0/4".to_owned(),
-    //     ];
-    // }
+    fn restict_to_public_ips(&mut self) {
+        self.peer.allowed_ips = vec![
+            "0.0.0.0/5".to_owned(),
+            "8.0.0.0/7".to_owned(),
+            "11.0.0.0/8".to_owned(),
+            "12.0.0.0/6".to_owned(),
+            "16.0.0.0/4".to_owned(),
+            "32.0.0.0/3".to_owned(),
+            "64.0.0.0/2".to_owned(),
+            "128.0.0.0/3".to_owned(),
+            "160.0.0.0/5".to_owned(),
+            "168.0.0.0/6".to_owned(),
+            "172.0.0.0/12".to_owned(),
+            "172.32.0.0/11".to_owned(),
+            "172.64.0.0/10".to_owned(),
+            "172.128.0.0/9".to_owned(),
+            "173.0.0.0/8".to_owned(),
+            "174.0.0.0/7".to_owned(),
+            "176.0.0.0/4".to_owned(),
+            "192.0.0.0/9".to_owned(),
+            "192.128.0.0/11".to_owned(),
+            "192.160.0.0/13".to_owned(),
+            "192.169.0.0/16".to_owned(),
+            "192.170.0.0/15".to_owned(),
+            "192.172.0.0/14".to_owned(),
+            "192.176.0.0/12".to_owned(),
+            "192.192.0.0/10".to_owned(),
+            "193.0.0.0/8".to_owned(),
+            "194.0.0.0/7".to_owned(),
+            "196.0.0.0/6".to_owned(),
+            "200.0.0.0/5".to_owned(),
+            "208.0.0.0/4".to_owned(),
+        ];
+    }
+}
+
+mod optional_csv_vec {
+    use super::*;
+
+    pub fn serialize<S, T>(option: &Option<Vec<T>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+        T: ToString,
+    {
+        match option {
+            Some(vec) => {
+                let csv = vec
+                    .iter()
+                    .map(|item| item.to_string())
+                    .collect::<Vec<_>>()
+                    .join(",");
+                serializer.serialize_str(&csv)
+            }
+            None => serializer.serialize_none(),
+        }
+    }
+
+    pub fn deserialize<'de, D, T>(deserializer: D) -> Result<Option<Vec<T>>, D::Error>
+    where
+        D: Deserializer<'de>,
+        T: std::str::FromStr,
+        T::Err: std::fmt::Display,
+    {
+        let s: Option<String> = Option::deserialize(deserializer)?;
+        s.map(|s| {
+            s.split(',')
+                .map(|item| item.trim().parse::<T>().map_err(serde::de::Error::custom))
+                .collect()
+        })
+        .transpose()
+    }
+}
+
+mod csv_vec {
+    use super::*;
+
+    pub fn serialize<S, T>(vec: &Vec<T>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+        T: ToString,
+    {
+        let csv = vec
+            .iter()
+            .map(|item| item.to_string())
+            .collect::<Vec<_>>()
+            .join(",");
+        serializer.serialize_str(&csv)
+    }
+
+    pub fn deserialize<'de, D, T>(deserializer: D) -> Result<Vec<T>, D::Error>
+    where
+        D: Deserializer<'de>,
+        T: std::str::FromStr,
+        T::Err: std::fmt::Display,
+    {
+        let s: String = String::deserialize(deserializer)?;
+        s.split(',')
+            .map(|item| item.trim().parse::<T>().map_err(serde::de::Error::custom))
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_wgprivatekey_creation_and_conversion() {
+        let private_key = WGPrivateKey::new();
+        let public_key = private_key.public();
+        assert_ne!(private_key.to_string(), public_key.to_string());
+    }
+
+    #[test]
+    fn test_wgprivatekey_serialization() {
+        let private_key = WGPrivateKey::new();
+        let serialized = serde_json::to_string(&private_key).unwrap();
+        let deserialized: WGPrivateKey = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(private_key, deserialized);
+    }
+
+    #[test]
+    fn test_wgconf_serialization_deserialization() {
+        let wg_conf = create_test_wgconf();
+        let serialized = wg_conf.to_ini().expect("Failed to serialize WGConf");
+        let deserialized = WGConf::from_ini(serialized).expect("Failed to deserialize WGConf");
+
+        assert_eq!(
+            wg_conf, deserialized,
+            "Serialized and deserialized WGConf should be equal"
+        );
+    }
+    #[test]
+    fn test_wgconf_serialization_with_disabled_dns() {
+        let mut wg_conf = create_test_wgconf();
+        wg_conf.disable_dns();
+        let serialized = wg_conf.to_ini().unwrap();
+        assert!(!serialized.contains("DNS"));
+        let deserialized = WGConf::from_ini(serialized).unwrap();
+        assert_eq!(wg_conf, deserialized);
+    }
+
+    #[test]
+    fn test_wgconf_serialization_with_restricted_public_ips() {
+        let mut wg_conf = create_test_wgconf();
+        wg_conf.restict_to_public_ips();
+        let serialized = wg_conf.to_ini().unwrap();
+        assert!(serialized.contains("0.0.0.0/5,8.0.0.0/7,11.0.0.0/8"));
+        let deserialized = WGConf::from_ini(serialized).unwrap();
+        assert_eq!(wg_conf, deserialized);
+    }
+
+    fn create_test_wgconf() -> WGConf {
+        WGConf {
+            interface: WGConfInterface {
+                address: IpAddr::from_str("10.0.0.1").unwrap(),
+                private_key: WGPrivateKey::new(),
+                dns: Some(vec![
+                    IpAddr::from_str("10.0.0.1").unwrap(),
+                    IpAddr::from_str("10.0.0.2").unwrap(),
+                ]),
+            },
+            peer: WGConfPeer {
+                public_key: WGPublicKey::from(&WGPrivateKey::new()),
+                allowed_ips: vec!["0.0.0.0/0".to_owned()],
+                endpoint: SocketAddr::from_str("10.0.0.128:5336").unwrap(),
+                hostname: Some("test-server".to_owned()),
+                persistent_keepalive: Some(25),
+            },
+        }
+    }
 }
